64091ab89d53c995ac8cc7caded814443f10d1a4
Anubhav Bagri <anubhav.199302029@muj.manipal.edu>
2022-07-07 03:36:22 +0530

    [FEAT] firing event sfrom UI

--- lib/screens/sign_in/sign_in_screen.dart
+++ lib/screens/sign_in/sign_in_screen.dart
@@ -1,7 +1,15 @@
+import 'package:bloc_validation/screens/sign_in/bloc/sign_in_bloc.dart';
+import 'package:bloc_validation/screens/sign_in/bloc/sign_in_event.dart';
+import 'package:bloc_validation/screens/sign_in/bloc/sign_in_state.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class SignInScreen extends StatelessWidget {
-  const SignInScreen({Key? key}) : super(key: key);
+  SignInScreen({Key? key}) : super(key: key);
+
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -18,17 +26,72 @@ class SignInScreen extends StatelessWidget {
           ),
           child: ListView(
             physics: const NeverScrollableScrollPhysics(),
-            children: const [
-              Text(
-                "Error will show up here",
-                style: TextStyle(
-                  color: Colors.red,
+            children: [
+              BlocBuilder<SignInBloc, SignInState>(
+                builder: (context, state) {
+                  if (state is SignInErrorState) {
+                    return Text(
+                      state.errorMessage,
+                      style: const TextStyle(
+                        color: Colors.red,
+                      ),
+                    );
+                  } else {
+                    return const SizedBox.shrink();
+                  }
+                },
+              ),
+              vSizedBox,
+              TextField(
+                controller: emailController,
+                onChanged: (val) {
+                  BlocProvider.of<SignInBloc>(context).add(
+                    SignInTextChangedEvent(
+                        emailController.text, passwordController.text),
+                  );
+                },
+                decoration: const InputDecoration(
+                  hintText: "Email Address",
                 ),
               ),
-              SizedBox(
-                height: 10,
+              vSizedBox,
+              TextField(
+                controller: passwordController,
+                onChanged: (val) {
+                  BlocProvider.of<SignInBloc>(context).add(
+                    SignInTextChangedEvent(
+                        emailController.text, passwordController.text),
+                  );
+                },
+                decoration: const InputDecoration(
+                  hintText: "Password",
+                ),
+                obscureText: true,
+                // obscuringCharacter: '*',
               ),
-              TextField()
+              const SizedBox(
+                height: 20,
+              ),
+              BlocBuilder<SignInBloc, SignInState>(
+                builder: (context, state) {
+                  if (state is SignInLoadingState) {
+                    return const Center(child: CircularProgressIndicator());
+                  }
+                  return CupertinoButton(
+                    onPressed: () {
+                      if (state is SignInValidState) {
+                        BlocProvider.of<SignInBloc>(context).add(
+                          SignInSubmittedEvent(
+                              emailController.text, passwordController.text),
+                        );
+                      }
+                    },
+                    color:
+                        (state is SignInValidState) ? Colors.blue : Colors.grey,
+                    child: const Text('Sign In'),
+                  );
+                },
+              )
             ],
           ),
         ),
@@ -36,3 +99,7 @@ class SignInScreen extends StatelessWidget {
     );
   }
 }
+
+const vSizedBox = SizedBox(
+  height: 10,
+);
